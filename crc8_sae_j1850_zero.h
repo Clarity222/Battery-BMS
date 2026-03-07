c6ae67e557a9d6d11ed3ec2ea127ef4d5abd03d2
Clarity222 <bysessa@gmail.com>
2026-03-07 15:36:39 -0500

    Add files via upload

---
+++ crc8_sae_j1850_zero.h
@@ -0,0 +1,25 @@
+#ifndef CRC8_SAE_J1850_ZERO_H
+#define CRC8_SAE_J1850_ZERO_H
+
+#include <Arduino.h>
+
+class CRC8_SAE_J1850_ZERO
+{
+public:
+    static uint8_t calculate(const uint8_t *data, uint8_t len);
+
+private:
+    static const uint8_t crc8_table[256];
+};
+
+class Counter
+{
+public:
+    Counter();
+    uint8_t increment();
+
+private:
+    uint8_t count;
+};
+
+#endif
